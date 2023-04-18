7b4dd618ca3d46037208504fc206d263553db15f
pierrepetrella <pierrepetrella>
2023-04-18 17:32:12 +0100

    Edited files

--- jython-processors/add-record-id/processor.py
+++ jython-processors/add-record-id/processor.py
@@ -1,21 +1,13 @@
 # This file is the actual code for the custom Jython step add-record-id
-import dataiku
-import os, json
-from dataiku.customstep import *
-from io_utils import *
-import re
-
-resource_folder = get_step_resource()
-plugin_config = get_plugin_config()
-step_config = get_step_config()
-
 
 start_index_at_1 = step_config.get["start_index_at_1"]
 
 # Define here a function that returns the result of the step.
 count = 0
 def process(row):
+    start_index_at_1 = params.get('start_index_at_1')
     global record_id
+    
     record_id = count
     if start_index_at_1:
         record_id +=1
