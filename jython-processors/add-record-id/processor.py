280bce7ac0927cd970fb8dd7bbcc6c46806637e1
pierrepetrella <pierrepetrella>
2023-04-18 17:46:42 +0100

    Edited files

--- jython-processors/add-record-id/processor.py
+++ jython-processors/add-record-id/processor.py
@@ -1,8 +1,5 @@
 # This file is the actual code for the custom Jython step add-record-id
 
-
-# Define here a function that returns the result of the step.
-
 count = 0
 def process(row):
     global count
