a79d8ff75bcd5c1ce5ced7f9c54216e3237aed62
pierrepetrella <pierrepetrella>
2023-04-18 17:40:29 +0100

    Edited files

--- jython-processors/add-record-id/processor.py
+++ jython-processors/add-record-id/processor.py
@@ -2,14 +2,12 @@
 
 
 # Define here a function that returns the result of the step.
-if params.get('start_index_at_1'):
-    record_id = 0
-else:
-    record_id = -1
 
+count = 0
 def process(row):
-    global record_id
-    record_id =+1
-    
-    
+    global count
+    record_id = count
+    if params.get('start_index_at_1'):
+        record_id +=1
+    count +=1  
     return record_id
